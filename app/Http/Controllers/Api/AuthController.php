6a6a2e1b4dcc21ab279c8dd06e36615fb86988cc
alimasudd <alimasudd.dev@gmail.com>
2023-12-19 10:19:26 +0700

    Create api login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -45,5 +46,34 @@ class AuthController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response([
+                'message' => ['Email not found'],
+            ], 404);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['Password is wrong'],
+            ], 404);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
     }
 }
