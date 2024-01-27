d2269873212985ac7c63850cd5b67f3d120d6041
alimasudd <alimasudd.dev@gmail.com>
2024-01-27 19:57:00 +0700

    modified login.blade

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.auth')
-
 @section('title', 'Login')
-
 @push('style')
     <!-- CSS Libraries -->
     <link rel="stylesheet" href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
